2ecbc85497ad02c2ff01237908d0d3a7d3f4e869
Gaurav9695328446 <gauravmka24@gmail.com>
2023-12-31 15:21:35 +0530

    largest-substring-between-equal-solution

--- 1624.largest-substring-between-two-characters-are-equal.cpp
+++ 1624.largest-substring-between-two-characters-are-equal.cpp
@@ -1,19 +1,50 @@
+Problem: Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+
+A substring is a contiguous sequence of characters within a string.
+
+Input: s = "aa"
+Output: 0
+Explanation: The optimal substring here is an empty substring between the two 'a's.
+
+Input: s = "abca"
+Output: 2
+Explanation: The optimal substring here is "bc".
+
+Input: s = "cbzxy"
+Output: -1
+Explanation: There are no characters that appear twice in s.
+
+
+Constraints:
+
+1 <= s.length <= 300
+s contains only lowercase English letters.
+
+
 #include<bits/stdc++.h>
+
+// Time complexity : O(n2)
+// Space complexity : O(1)
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        //brute force O(n2)
-        //         int ans = -1;
-        // for (int i = 0; i < s.size(); i++) {
-        //     for (int j = i + 1; j < s.size(); j++) {
-        //         if (s[i] == s[j]) {
-        //             ans = max(ans, (j - i) - 1);
-        //         }
-        //     }
-        // }
+        int ans = -1;
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = i + 1; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    ans = max(ans, (j - i) - 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
 
-        // return ans;
-
+// Time complexity : O(n)
+// Space complexity : O(1)
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
         unordered_map<char, int> firstIndex;
         int ans = -1;
         
@@ -27,5 +58,4 @@ public:
 
         return ans;
     }
-};
-
+}
