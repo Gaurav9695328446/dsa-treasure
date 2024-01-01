239fbefa7a256df3e58d8c1188111629a32cd5db
Gaurav9695328446 <gauravmka24@gmail.com>
2024-01-01 14:00:23 +0530

    folder structure updated

--- 2023/dec/31-12-23.cpp
+++ 2023/dec/31-12-23.cpp
@@ -1,3 +1,7 @@
+/*
+Title: Largest Substring Between Two Equal Characters
+Link: https://leetcode.com/problems/largest-substring-between-two-equal-characters/description/
+
 Problem: Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
 
 A substring is a contiguous sequence of characters within a string.
@@ -19,6 +23,7 @@ Constraints:
 
 1 <= s.length <= 300
 s contains only lowercase English letters.
+*/
 
 
 #include<bits/stdc++.h>
