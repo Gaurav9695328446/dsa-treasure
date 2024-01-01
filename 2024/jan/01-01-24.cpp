42415243e873521bcb8b689a3e25dcbeeffe5bd9
Gaurav9695328446 <gauravmka24@gmail.com>
2024-01-01 16:52:21 +0530

    solution updated

--- 2024/jan/01-01-24.cpp
+++ 2024/jan/01-01-24.cpp
@@ -34,24 +34,16 @@ Constraints:
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cookiesNums = s.size();
-        if(cookiesNums == 0)  return 0;
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int maxNum = 0;
-        int cookieIndex = cookiesNums - 1;
-        int childIndex = g.size() - 1;
-        while(cookieIndex >= 0 && childIndex >=0){
-            if(s[cookieIndex] >= g[childIndex]){
-                maxNum++;
-                cookieIndex--;
-                childIndex--;
+        int i = 0, j = 0;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                i++;
             }
-            else{
-                childIndex--;
-            }
+            j++;
         }
-        return maxNum;
+        return i;
     }
 };
