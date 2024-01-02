0e2753f67459087b9a7fc4ea77d02542a317a839
Gaurav9695328446 <gauravmka24@gmail.com>
2024-01-02 21:13:04 +0530

    jan 02 solution passed

---
+++ 2024/jan/02-01-2024.cpp
@@ -0,0 +1,70 @@
+/* 
+
+Title: Convert an Array Into a 2D Array With Conditions
+Link: https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/?envType=daily-question&envId=2024-01-02
+
+Problem: You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+
+The 2D array should contain only the elements of the array nums.
+Each row in the 2D array contains distinct integers.
+The number of rows in the 2D array should be minimal.
+Return the resulting array. If there are multiple answers, return any of them.
+
+Note that the 2D array can have a different number of elements on each row.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,1,2,3,1]
+Output: [[1,3,4,2],[1,3],[1]]
+Explanation: We can create a 2D array that contains the following rows:
+- 1,3,4,2
+- 1,3
+- 1
+All elements of nums were used, and each row of the 2D array contains distinct integers, so it is a valid answer.
+It can be shown that we cannot have less than 3 rows in a valid array.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: [[4,3,2,1]]
+Explanation: All elements of the array are distinct, so we can keep all of them in the first row of the 2D array.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= nums.length
+ */
+
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> ans;
+        unordered_map<int, int> mpp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            mpp[nums[i]]++;
+        }
+
+        while (!mpp.empty()) {
+            vector<int> row;
+            for (auto it = mpp.begin(); it != mpp.end();) {
+                if (it->second != 0) {
+                    row.push_back(it->first);
+                    it->second--;
+                }
+                //code to remember for map in c++
+                if (it->second == 0) {
+                    it = mpp.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
