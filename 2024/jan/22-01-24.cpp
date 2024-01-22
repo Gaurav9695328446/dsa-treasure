cb92e6184878d18523019d29ee05b1e53484c43a
Gaurav9695328446 <gauravmka24@gmail.com>
2024-01-23 00:08:18 +0530

    22 jan code pushed

--- 2024/jan/22-01-24.cpp
+++ 2024/jan/22-01-24.cpp
@@ -1,48 +1,53 @@
 /*
-Title. House Robber
-URL: https://leetcode.com/problems/house-robber/description/?envType=daily-question&envId=2024-01-21
+Title. Set Mismatch
+URL: https://leetcode.com/problems/set-mismatch/description/?envType=daily-question&envId=2024-01-22
 
 
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
  
 Example 1:
-
-Input: nums = [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-Total amount you can rob = 1 + 3 = 4.
-
+Input: nums = [1,2,2,4]
+Output: [2,3]
 Example 2:
-
-Input: nums = [2,7,9,3,1]
-Output: 12
-Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-Total amount you can rob = 2 + 9 + 1 = 12.
+Input: nums = [1,1]
+Output: [1,2]
 
  
 Constraints:
 
-	1 <= nums.length <= 100
-	0 <= nums[i] <= 400
+	2 <= nums.length <= 104
+	1 <= nums[i] <= 104
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int rob = 0;
-        int norob = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int newRob = norob + nums[i];
-            int newNoRob = max(norob, rob);
-            rob = newRob;
-            norob = newNoRob;
+    std::vector<int> findErrorNums(std::vector<int>& nums) {
+        int dup = -1, missing = -1;
+        
+        for (int i = 1; i <= nums.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                dup = i;
+            } else if (count == 0) {
+                missing = i;
+            }
         }
-        return max(rob, norob);
+        
+        return {dup, missing};
     }
 };
