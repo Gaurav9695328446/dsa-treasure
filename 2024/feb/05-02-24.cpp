28eb0279e75692b23a44ac920f34f77ea32350fd
Gaurav9695328446 <gauravmka24@gmail.com>
2024-02-06 00:08:23 +0530

    5 feb leetcode sol pushed

--- 2024/feb/05-02-24.cpp
+++ 2024/feb/05-02-24.cpp
@@ -1,42 +1,26 @@
 /*
-URL: https://leetcode.com/problems/minimum-window-substring/description/?envType=daily-question&envId=2024-02-04
+Title: First Unique Character in a String
+URL: https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=daily-question&envId=2024-02-05
 
-76. Minimum Window Substring
 
-Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
-
-The testcases will be generated such that the answer is unique.
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
  
 Example 1:
-
-Input: s = "ADOBECODEBANC", t = "ABC"
-Output: "BANC"
-Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
-
+Input: s = "leetcode"
+Output: 0
 Example 2:
-
-Input: s = "a", t = "a"
-Output: "a"
-Explanation: The entire string s is the minimum window.
-
+Input: s = "loveleetcode"
+Output: 2
 Example 3:
-
-Input: s = "a", t = "aa"
-Output: ""
-Explanation: Both 'a's from t must be included in the window.
-Since the largest window of s only has one 'a', return empty string.
+Input: s = "aabb"
+Output: -1
 
  
 Constraints:
 
-	m == s.length
-	n == t.length
-	1 <= m, n <= 105
-	s and t consist of uppercase and lowercase English letters.
-
- 
-Follow up: Could you find an algorithm that runs in O(m + n) time? 
+	1 <= s.length <= 105
+	s consists of only lowercase English letters.
 */
 
 #include<bits/stdc++.h>
@@ -44,28 +28,16 @@ using namespace std;
 
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for (char c : t) {
-            map[c]++;
+    int firstUniqChar(string s) {
+        vector<int> oc(26);
+        //store frequency of each character of s
+        for (auto i : s) oc[i - 'a']++;
+        //first character with frequency = 1 is the answer
+        for (int i = 0; i < s.size(); i++) {
+            if (oc[s[i] - 'a'] == 1) return i;
         }
-
-        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
-        while (end < s.size()){
-            if (map[s[end++]]-- > 0) {
-                counter--;
-            }
-            while (counter == 0) {
-                if (end - begin < d) {
-                    head = begin;
-                    d = end - head;
-                }
-                if (map[s[begin++]]++ == 0) {
-                    counter++;
-                }
-            }  
-        }
-        return d == INT_MAX ? "" : s.substr(head, d);
+        //no character with frequency = 1
+        return -1;
     }
 };
 
