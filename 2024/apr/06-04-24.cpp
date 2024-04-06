06912229678c172833fce969959b5abc7ef5a8be
Gaurav9695328446 <gauravmka24@gmail.com>
2024-04-06 23:30:07 +0530

    6 apr leetcode sol pushed

---
+++ 2024/apr/06-04-24.cpp
@@ -0,0 +1,83 @@
+/*
+URL: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/description/?envType=daily-question&envId=2024-04-06
+
+1249. Minimum Remove to Make Valid Parentheses
+
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+	It is the empty string, contains only lowercase characters, or
+	It can be written as AB (A concatenated with B), where A and B are valid strings, or
+	It can be written as (A), where A is a valid string.
+
+ 
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+ 
+Constraints:
+
+	1 <= s.length <= 105
+	s[i] is either'(' , ')', or lowercase English letter.
+*/
+
+
+
+class Solution {
+public:
+    std::string minRemoveToMakeValid(std::string s) {
+        int leftCount = 0;
+        int rightCount = 0;
+        std::stack<char> stack;
+
+        // Pass 1
+        for (char ch : s) {
+            if (ch == '(') {
+                leftCount++;
+            } else if (ch == ')') {
+                rightCount++;
+            }
+            if (rightCount > leftCount) {
+                rightCount--;
+                continue;
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        std::string result = "";
+        
+        // Pass 2
+        while (!stack.empty()) {
+            char currentChar = stack.top();
+            stack.pop();
+            if (leftCount > rightCount && currentChar == '(') {
+                leftCount--;
+            } else {
+                result += currentChar;
+            }
+        }
+
+        // Reverse the result string
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
