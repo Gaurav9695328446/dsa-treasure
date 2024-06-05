e6fc4e0601a63efabfabd5a3ab88a1da09e7fc6f
Gaurav9695328446 <gauravmka24@gmail.com>
2024-06-05 21:25:10 +0530

    5 june sol pushed

---
+++ 2024/june/05-06-24.cpp
@@ -0,0 +1,64 @@
+/*
+URL: https://leetcode.com/problems/find-common-characters/description/?envType=daily-question&envId=2024-06-05
+
+1002. Find Common Characters
+
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+ 
+Example 1:
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+ 
+Constraints:
+
+	1 <= words.length <= 100
+	1 <= words[i].length <= 100
+	words[i] consists of lowercase English letters.
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> last = count(words[0]);
+        for (int i = 1; i < words.size(); i++) {
+            last = intersection(last, count(words[i]));
+        }
+        
+        vector<string> result;
+        for (int i = 0; i < 26; i++) {
+            while (last[i] > 0) {
+                result.push_back(string(1, 'a' + i));
+                last[i]--;
+            }
+        }
+        
+        return result;
+    }
+    
+private:
+    vector<int> count(const string& str) {
+        vector<int> frequency(26, 0);
+        for (char c : str) {
+            frequency[c - 'a']++;
+        }
+        return frequency;
+    }
+    
+    vector<int> intersection(const vector<int>& a, const vector<int>& b) {
+        vector<int> t(26, 0);
+        for (int i = 0; i < 26; i++) {
+            t[i] = min(a[i], b[i]);
+        }
+        return t;
+    }
+};
