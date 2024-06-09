0d6eeb742b904613711e1a858f68a549ac2d75a1
Gaurav9695328446 <gauravmka24@gmail.com>
2024-06-10 00:02:19 +0530

    9 june leetcode sol pushed

---
+++ 2024/june/09-06-24.cpp
@@ -0,0 +1,68 @@
+/*
+URL: https://leetcode.com/problems/subarray-sums-divisible-by-k/description/?envType=daily-question&envId=2024-06-09
+
+974. Subarray Sums Divisible by K
+
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+ 
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+
+ 
+Constraints:
+
+	1 <= nums.length <= 3 * 104
+	-104 <= nums[i] <= 104
+	2 <= k <= 104
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int subarraysDivByK(std::vector<int>& nums, int k) {
+        // Initialize count of subarrays, prefix sum, and hash map for remainders
+        int count = 0;
+        int prefixSum = 0;
+        std::unordered_map<int, int> prefixMap;
+        prefixMap[0] = 1; // To handle subarrays that start from the beginning
+
+        for (int num : nums) {
+            // Update prefix sum
+            prefixSum += num;
+            
+            // Calculate the remainder of the prefix sum divided by k
+            int mod = prefixSum % k;
+            
+            // Adjust negative remainders to be positive
+            if (mod < 0) {
+                mod += k;
+            }
+            
+            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
+            if (prefixMap.find(mod) != prefixMap.end()) {
+                count += prefixMap[mod];
+                prefixMap[mod] += 1;
+            } else {
+                prefixMap[mod] = 1;
+            }
+        }
+        
+        return count; // Total number of subarrays divisible by k
+    }
+};
